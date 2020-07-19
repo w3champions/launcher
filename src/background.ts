5b0587dd121b2e6b07815a6d2faf460f0e4817c2
Simon Heiss <simonheiss87@gmail.com>
2020-07-19 15:02:30 +0200

    fn awesome

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,6 @@
 
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-// @ts-ignore
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
