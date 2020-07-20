062ea71df7f3d4564cb85235a40d5cee886f6e38
Simon Heiss <simonheiss87@gmail.com>
2020-07-20 17:36:08 +0200

    new icon

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,3 @@ import W3CLauncher from './components/W3CLauncher.vue';
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
