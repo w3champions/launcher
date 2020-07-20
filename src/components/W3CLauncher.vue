62a04477d2f8ada0b92f7bf00fc770745455222f
Simon Heiss <simonheiss87@gmail.com>
2020-07-20 23:40:31 +0200

    kind of working, storage not persisting, though

--- src/components/W3CLauncher.vue
+++ src/components/W3CLauncher.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <button @click="startWc3">Start w3c!</button>
+    <button @click="tryStartWc3">Start w3c!</button>
+    <div>w3c: {{lastSavedwc3Path}}</div>
+    <div>maps: {{lastSavedMapPath}}</div>
   </div>
 </template>
 
@@ -14,46 +16,61 @@ export default class W3CLauncher extends Vue {
   private wc3PathKey = "wc3PathKey";
   private wc3MapKey = "wc3MapKey";
 
-  private wc3Path = "C:\\Program Files (x86)\\Warcraft III";
-  private wc3MapPath = "C:\\Users\\simon\\Documents\\Warcraft III\\Maps";
+  private wc3DefaultPathWindows = "C:\\Program Files (x86)\\Warcraft III";
+  private wc3DefaultPatMapPathWindows = "Warcraft III\\Maps";
 
-  public startWc3() {
-    this.updateIfNeeded();
+  public lastSavedwc3Path = ""
+  public lastSavedMapPath = ""
+
+
+  async mounted() {
+    if (await storage.isPathExists(this.wc3PathKey)) {
+      this.lastSavedwc3Path = await storage.get(this.wc3PathKey);
+    }
+    if (await storage.isPathExists(this.wc3MapKey)) {
+      this.lastSavedMapPath = await storage.get(this.wc3MapKey);
+    }
+  }
+
+  public tryStartWc3() {
+    const success = this.updateIfNeeded();
+    if (!success) return;
+    this.startWc3();
   }
 
   private async updateIfNeeded() {
     if (!this.lastSavedwc3Path) {
       const folder = await this.GetFolderFromUser('Warcraft III not found', 'Warcraft III folder not found, please locate it manually');
-      storage.set(this.wc3PathKey, folder);
+      if (!folder) return false;
+      await storage.set(this.wc3PathKey, folder);
+      this.lastSavedwc3Path = folder;
     }
 
     if (!this.lastSavedMapPath) {
       const folder = await this.GetFolderFromUser('Mapfolder not found', 'The mapfolder of Warcraft III was not found, please locate it manually');
-      storage.set(this.wc3PathKey, folder);
+      if (!folder) return false;
+      await storage.set(this.wc3PathKey, folder);
+      this.lastSavedMapPath = folder;
     }
 
-    storage.set()
+    return true;
   }
 
   private async GetFolderFromUser(title: string, message: string) {
     const folderResult = await remote.dialog.showMessageBox(null, {
       title: title,
       message: message,
-      buttons: ['Locate Folder', 'Cancel Installation']
+      buttons: ['Locate Folder', 'Cancel Update']
     })
     if (folderResult.response === 1) {
-      window.close()
+      return ""
     }
     const openDialogReturnValue = await remote.dialog.showOpenDialog({properties: ['openDirectory', 'multiSelections']});
     return openDialogReturnValue.filePaths[0];
   }
 
-  get lastSavedwc3Path() {
-    return storage.get(this.wc3PathKey);
-  }
-
-  get lastSavedMapPath() {
-    return storage.get(this.wc3MapKey);
+  private startWc3() {
+    //tbd
   }
 }
 </script>
