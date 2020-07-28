e221d0ad32606e86b08e74b0763dffcdbaca171b
Simon Heiss <simonheiss87@gmail.com>
2020-07-28 20:49:20 +0200

    reinstalled electron builder

--- src/background.ts
+++ src/background.ts
@@ -28,7 +28,7 @@ function createWindow() {
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
