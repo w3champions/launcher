b7fdec955000eba89db2f97d334c4dba4ee1d4f4
Simon Heiss <simonheiss87@gmail.com>
2020-07-29 19:28:31 +0200

    smaller vue config, not needed the node modules was

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      externals: ['adm-zip'],
-      nodeModulesPath: ['../../node_modules', './node_modules']
+      externals: ['adm-zip']
     }
   }
 }
