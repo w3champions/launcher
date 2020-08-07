9aacce9f269ab08db28658d1588b6fe8d4bef2e6
Simon Heiss <simonheiss87@gmail.com>
2020-08-07 19:51:19 +0200

    abi on 80 like in doku

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,17 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       externals: ['adm-zip', 'axios', 'robotjs', 'arraybuffer-to-buffer'],
+      builderOptions: {
+        afterSign: "notarize.js",
+        mac: {
+          hardenedRuntime : true,
+          gatekeeperAssess: false,
+          entitlements: "build/entitlements.mac.inherit.plist"
+        },
+        dmg: {
+          sign: false
+        }
+      }
     }
   }
 }
