453c3e9caf0c4cab32085e80e5a3cf24f781f52c
Simon Heiss <simonheiss87@gmail.com>
2020-08-07 19:33:30 +0200

    remove notarize for test

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,6 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       externals: ['adm-zip', 'axios', 'robotjs', 'arraybuffer-to-buffer'],
-      builderOptions: {
-        afterSign: "notarize.js",
-        mac: {
-          hardenedRuntime : true,
-          gatekeeperAssess: false,
-          entitlements: "build/entitlements.mac.inherit.plist"
-        },
-        dmg: {
-          sign: false
-        }
-      }
     }
   }
 }
