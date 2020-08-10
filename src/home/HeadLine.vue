603fadfdea41f06b48e041bb47d13cf11380782c
Simon Heiss <simonheiss87@gmail.com>
2020-08-10 09:37:05 +0200

    settings button nice

--- src/home/HeadLine.vue
+++ src/home/HeadLine.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="header-stuff">
     <HeadItem target="/" text="Home"/>
-    <HeadItem target="/Settings" text="Settings"/>
     <HeadItem target="/HotKeys" text="Hot Keys"/>
+    <SettingsButton/>
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from "vue-property-decorator";
 import HeadItem from "@/home/HeadItem.vue";
+import SettingsButton from "@/home/SettingsButton.vue";
 
 @Component({
-  components: {HeadItem}
+  components: {SettingsButton, HeadItem}
 })
 export default class HeadLine extends Vue {
 }
@@ -22,8 +23,12 @@ export default class HeadLine extends Vue {
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  vertical-align: baseline;
   width: 100%;
-  height: 10vh;
+  height: 12vh;
+
+  background: url("~@/assets/images/modal/header.png") center no-repeat;
+  background-size: cover;
 }
 
 </style>
---
+++ src/home/SettingsButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div @click="goToTarget" class="settings-button">
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class SettingsButton extends Vue {
+  public goToTarget() {
+    this.$router.push({
+      path: "/Settings"
+    })
+  }
+}
+</script>
+
+<style scoped type="text/css">
+.settings-button {
+  width: 50px;
+  height: 50px;
+
+  background: url("~@/assets/images/icons/settings-icon-resting.png") center no-repeat;
+  background-size: cover;
+}
+
+.settings-button:hover {
+  background: url("~@/assets/images/icons/settings-icon-hover.png") center no-repeat;
+  background-size: cover;
+}
+
+</style>
