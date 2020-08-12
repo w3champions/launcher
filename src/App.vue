f4e73aaa572a5f160dc2e528bf3173b838d4ad66
Simon Heiss <simonheiss87@gmail.com>
2020-08-12 08:29:57 +0200

    launcher on startup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app" class="app-container">
+    <div class="new-launcher-version" v-if="hasNewLauncherVersion">
+      There is a new version of the launcher ({{ onlineLauncherVersion }})
+      <a class="download-button" :href="updateUrl">Download</a>
+    </div>
     <HeadLine />
     <div class="content-modal-wrapper">
       <div class="content-modal">
@@ -34,6 +38,25 @@ export default class App extends Vue {
     await this.$store.direct.dispatch.updateHandling.loadCurrentLauncherVersion();
     await this.$store.direct.dispatch.updateHandling.loadCurrentW3CVersion();
   }
+
+  get updateUrl() {
+
+    const isOs = os.platform() === "darwin" ? "mac" : "win"
+    return `${this.$store.direct.state.updateUrl}api/launchers/${isOs}`
+  }
+
+  get currentLauncherVersion(): string {
+    return this.$store.direct.state.updateHandling.localLauncherVersion
+  }
+
+  get onlineLauncherVersion() {
+    return this.$store.state.updateHandling.onlineLauncherVersion;
+  }
+
+  get hasNewLauncherVersion() {
+    return this.onlineLauncherVersion.localeCompare(this.currentLauncherVersion) === 0;
+  }
+
 
   private makeSureNumpadIsEnabled() {
     if (os.platform() === "darwin") {
@@ -71,6 +94,24 @@ body {
   color: antiquewhite;
 }
 
+.new-launcher-version {
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100vh;
+  font-size: 2em;
+  background: rgba(0, 0, 0, 0.8);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+a {
+  color: inherit;
+}
+
 .app-container {
   background: url("~@/assets/images/backgrounds/main_bg.png") center no-repeat;
   height: 99.5vh;
@@ -93,6 +134,33 @@ body {
   margin-top: 5vh;
   height: 76vh;
   width: 90%;
+}
+
+
+.download-button {
+  background-color: transparent;
+  text-transform: uppercase;
+  color: rgb(51, 38, 28);
+  background-image: linear-gradient(rgba(255, 255, 0, 0.2) 50%, transparent 50%),
+  linear-gradient(rgb(255, 209, 85), rgb(220, 166, 13));
+  box-shadow: rgba(0, 0, 0, 0.8) 0px 0px 0px 2px,
+  rgba(236, 174, 6, 0.3) 0px 0px 40px 15px,
+  rgba(255, 255, 255, 0.4) 0px 0px 0px 2px inset,
+  rgba(255, 125, 19, 0.3) 0px 0px 20px 10px inset;
+  text-shadow: rgb(51, 38, 28) 0px 0px;
+  height: 76px;
+  font-size: 26px;
+  line-height: 26px;
+  border-width: 0px;
+  border-style: initial;
+  border-color: initial;
+  border-image: initial;
+  border-radius: 2px;
+  background-repeat: no-repeat;
+  outline: 0px;
+  text-decoration: none;
+  transition: filter 200ms ease 0s;
+  padding: 0 95px;
 }
 
 </style>
