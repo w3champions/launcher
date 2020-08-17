0e2324ff80873d4d0594258944c07adf571b48f2
Simon Heiss <simonheiss87@gmail.com>
2020-08-17 21:33:28 +0200

    jeah

--- src/hot-keys/send-keys-native/binding.gyp
+++ src/hot-keys/send-keys-native/binding.gyp
@@ -1,14 +1,20 @@
 {
-  "targets": [
-    {
-      "cflags!": [ "-fno-exceptions" ],
-      "cflags_cc!": [ "-fno-exceptions" ],
-      "include_dirs" : [
-        "<!@(node -p \"require('node-addon-api').include\")"
-      ],
-      "target_name": "send-keys-native",
-      "sources": [ "send-keys.cc" ],
-      'defines': [ 'NAPI_DISABLE_CPP_EXCEPTIONS' ]
-    }
-  ]
-}
+    'targets': [
+      {
+        "cflags!": [ "-fno-exceptions" ],
+        "cflags_cc!": [ "-fno-exceptions" ],
+        "include_dirs" : [
+          "<!@(node -p \"require('node-addon-api').include\")"
+        ],
+        "target_name": "send-keys-native",
+        'defines': [ 'NAPI_DISABLE_CPP_EXCEPTIONS' ],
+        'conditions': [
+          ['OS=="win"', {
+            "sources": [ "send-keys-win.cc" ],
+          }, { # OS != "win",
+            "sources": [ "send-keys-osx.cc" ],
+          }]
+        ],
+      },
+    ],
+  }
---
+++ src/hot-keys/send-keys-native/send-keys-osx.cc
@@ -0,0 +1,80 @@
+#include <napi.h>
+
+#define VK_SPACE    0x20 // space key
+#define VK_ENTER    0x0D // enter key
+#define VK_ESCAPE   0x1B // escape key
+#define VK_F01      0x6F // f1 key
+#define VK_F02      0x70 // f2 key
+#define VK_F03      0x71 // f3 key
+#define VK_F10      0x79 // f10 key
+#define VK_F12      0x7B // f12 key
+
+Napi::Boolean ReleaseModifier(UINT modifier)
+{
+    return Napi::Boolean::Boolean();
+}
+
+Napi::Boolean ReleaseLControl(const Napi::CallbackInfo& info) { return ReleaseModifier(VK_LCONTROL); }
+Napi::Boolean ReleaseLAlt(const Napi::CallbackInfo& info) { return ReleaseModifier(VK_LMENU); }
+
+Napi::Boolean HoldModifier(UINT modifier)
+{
+    return Napi::Boolean::Boolean();
+}
+
+Napi::Boolean HoldLControl(const Napi::CallbackInfo& info) { return HoldModifier(VK_LCONTROL); }
+Napi::Boolean HoldLAlt(const Napi::CallbackInfo& info) { return HoldModifier(VK_LMENU); }
+
+Napi::Boolean PressKey(UINT key)
+{
+    return Napi::Boolean::Boolean();
+}
+
+Napi::Boolean PressNum8(const Napi::CallbackInfo& info) { return PressKey(VK_NUMPAD8); }
+Napi::Boolean PressNum7(const Napi::CallbackInfo& info) { return PressKey(VK_NUMPAD7); }
+Napi::Boolean PressNum5(const Napi::CallbackInfo& info) { return PressKey(VK_NUMPAD5); }
+Napi::Boolean PressNum4(const Napi::CallbackInfo& info) { return PressKey(VK_NUMPAD4); }
+Napi::Boolean PressNum2(const Napi::CallbackInfo& info) { return PressKey(VK_NUMPAD2); }
+Napi::Boolean PressNum1(const Napi::CallbackInfo& info) { return PressKey(VK_NUMPAD1); }
+Napi::Boolean PressNumlock(const Napi::CallbackInfo& info) { return PressKey(VK_NUMLOCK); }
+Napi::Boolean PressSpace(const Napi::CallbackInfo& info) { return PressKey(VK_SPACE); }
+
+Napi::Boolean PressEnter(const Napi::CallbackInfo& info) { return PressKey(VK_ENTER); }
+Napi::Boolean PressEscape(const Napi::CallbackInfo& info) { return PressKey(VK_ESCAPE); }
+Napi::Boolean PressF10(const Napi::CallbackInfo& info) { return PressKey(VK_F10); }
+Napi::Boolean PressF12(const Napi::CallbackInfo& info) { return PressKey(VK_F12); }
+
+Napi::Boolean PressF1(const Napi::CallbackInfo& info) { return PressKey(VK_F01); }
+Napi::Boolean PressF2(const Napi::CallbackInfo& info) { return PressKey(VK_F02); }
+Napi::Boolean PressF3(const Napi::CallbackInfo& info) { return PressKey(VK_F03); }
+
+
+Napi::Object init(Napi::Env env, Napi::Object exports) {
+    exports.Set(Napi::String::New(env, "pressNum1"), Napi::Function::New(env, PressNum1));
+    exports.Set(Napi::String::New(env, "pressNum2"), Napi::Function::New(env, PressNum2));
+    exports.Set(Napi::String::New(env, "pressNum4"), Napi::Function::New(env, PressNum4));
+    exports.Set(Napi::String::New(env, "pressNum5"), Napi::Function::New(env, PressNum5));
+    exports.Set(Napi::String::New(env, "pressNum7"), Napi::Function::New(env, PressNum7));
+    exports.Set(Napi::String::New(env, "pressNum8"), Napi::Function::New(env, PressNum8));
+
+    exports.Set(Napi::String::New(env, "pressNumLock"), Napi::Function::New(env, PressNumlock));
+    exports.Set(Napi::String::New(env, "pressSpace"), Napi::Function::New(env, PressSpace));
+
+    exports.Set(Napi::String::New(env, "pressEnter"), Napi::Function::New(env, PressEnter));
+    exports.Set(Napi::String::New(env, "pressEscape"), Napi::Function::New(env, PressEscape));
+    exports.Set(Napi::String::New(env, "pressF10"), Napi::Function::New(env, PressF10));
+    exports.Set(Napi::String::New(env, "pressF12"), Napi::Function::New(env, PressF12));
+
+    exports.Set(Napi::String::New(env, "pressF1"), Napi::Function::New(env, PressF1));
+    exports.Set(Napi::String::New(env, "pressF2"), Napi::Function::New(env, PressF2));
+    exports.Set(Napi::String::New(env, "pressF3"), Napi::Function::New(env, PressF3));
+
+    exports.Set(Napi::String::New(env, "holdCtrl"), Napi::Function::New(env, HoldLControl));
+    exports.Set(Napi::String::New(env, "holdAlt"), Napi::Function::New(env, HoldLAlt));
+    exports.Set(Napi::String::New(env, "releaseCtrl"), Napi::Function::New(env, ReleaseLControl));
+    exports.Set(Napi::String::New(env, "releaseAlt"), Napi::Function::New(env, ReleaseLAlt));
+
+    return exports;
+};
+
+NODE_API_MODULE(send_keys_native, init);
