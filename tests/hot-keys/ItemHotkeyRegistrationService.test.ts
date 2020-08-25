ea96f7b28d89fd092cce4b13bb34bc66bdab3da1
Simon Heiss <simonheiss87@gmail.com>
2020-08-25 17:04:05 +0200

    shit

--- tests/hot-keys/ItemHotkeyRegistrationService.test.ts
+++ tests/hot-keys/ItemHotkeyRegistrationService.test.ts
@@ -1,8 +1,4 @@
 import {InGameState, NotInGameState} from "@/hot-keys/HotKeyStateMachine";
-// @ts-ignore
-window = {
-    require: require
-};
 
 test('Transfer from f10 over escape to ingame with enabled keys', () => {
     const service = new NotInGameState()
