2f746a9fc49be437a972e3af984ef432532f4a1e
Simon Heiss <simonheiss87@gmail.com>
2020-08-31 17:56:12 +0200

    fix another log error now for real

--- src/update-handling/LauncherStrategy.ts
+++ src/update-handling/LauncherStrategy.ts
@@ -159,7 +159,7 @@ export abstract class LauncherStrategy{
     }
 
     get w3PathWithOutRetail() {
-        return this.w3Path?.replace("/_retail_", "").replace("\\_retail_", "") ?? "";
+        return this.w3Path?.replace("/_retail_", "")?.replace("\\_retail_", "") ?? "";
     }
 
     private async downloadAndWriteFile(fileName: string, to: string, onProgress?: (percentage: number) => void) {
