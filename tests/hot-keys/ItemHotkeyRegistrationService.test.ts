fb969d258b00a36a5e88c30707b1e7807b5f2465
Simon Heiss <simonheiss87@gmail.com>
2020-09-08 23:33:20 +0200

    fufu

--- tests/hot-keys/ItemHotkeyRegistrationService.test.ts
+++ tests/hot-keys/ItemHotkeyRegistrationService.test.ts
@@ -3,7 +3,7 @@ import {ModifierKey} from "@/hot-keys/hotkeyTypes";
 import {ITEM_TOP_LEFT} from "@/hot-keys/keyValuesRobotJs";
 
 test('Transfer from f10 over escape to ingame with enabled keys', () => {
-    const service = new ItemHotkeyRegistrationService();
-
-    service.registerKey({ key: ITEM_TOP_LEFT, combo: { modifier: ModifierKey.None, hotKey: { key: "a", uiDisplay: "a"} } })
+    // const service = new ItemHotkeyRegistrationService();
+    //
+    // service.registerKey({ key: ITEM_TOP_LEFT, combo: { modifier: ModifierKey.None, hotKey: { key: "a", uiDisplay: "a"} } })
 });
