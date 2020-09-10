750561b112fdbc814afda1c1f8fc5c06bac72f7f
Simon Heiss <simonheiss87@gmail.com>
2020-09-11 01:37:19 +0200

    hks jeah

--- src/hot-keys/views/RaceTab.vue
+++ src/hot-keys/views/RaceTab.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    {{race}}
+  <div class="center-thing">
+    <div>
+      coming soon...
+    </div>
   </div>
 </template>
 
@@ -18,4 +20,11 @@ export default class RaceTab extends Vue {
 </script>
 
 <style scoped type="text/css">
+.center-thing {
+  width: 100%;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
