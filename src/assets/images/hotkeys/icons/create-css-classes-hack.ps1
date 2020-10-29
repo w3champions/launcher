8ff03f93514744436f84c75f44bbfa96d65fca55
Simon Heiss <simonheiss87@gmail.com>
2020-10-29 21:27:06 +0100

    all images in one place to not move the stuff around

--- src/assets/images/hotkeys/icons/create-css-classes-hack.ps1
+++ src/assets/images/hotkeys/icons/create-css-classes-hack.ps1
@@ -1,21 +1,13 @@
 $hackFile = ".\hack-classes.txt"
 
-function writeClasses($folder){
-    $pictures = Get-ChildItem -Path "./$folder"
-    foreach ($pic in $pictures){
-        $picName = $pic.Name.split(".")[0]
-
-        Add-Content $hackFile ".$folder-$picName {"
-        Add-Content $hackFile "  background: url('~@/assets/images/hotkeys/icons/$folder/$picName.jpg') no-repeat center;"
-        Add-Content $hackFile "  background-size: cover;"
-        Add-Content $hackFile "}`n"
-    }
-}
-
 New-Item $hackFile -Force
 
-writeClasses('human')
-writeClasses('orc')
-writeClasses('undead')
-writeClasses('nightelf')
-writeClasses('neutral')
+$pictures = Get-ChildItem -Path "./all"
+foreach ($pic in $pictures){
+    $picName = $pic.Name.split(".")[0]
+
+    Add-Content $hackFile ".$picName {"
+    Add-Content $hackFile "  background: url('~@/assets/images/hotkeys/icons/all/$picName.jpg') no-repeat center;"
+    Add-Content $hackFile "  background-size: cover;"
+    Add-Content $hackFile "}`n"
+}
