cd568cd1fea4e4723c1dc549205dde4b77a9763d
Kenshin <kibakenshin@gmail.com>
2020-11-04 18:30:28 +0100

    human is dead

--- src/hot-keys/RaceSpecificHotkeys/hotkeyData/human/buildings.ts
+++ src/hot-keys/RaceSpecificHotkeys/hotkeyData/human/buildings.ts
@@ -3,16 +3,32 @@ import {
     Building,
     Unit,
 } from "@/hot-keys/RaceSpecificHotkeys/raceSpecificHotkeyTypes";
-import {cancelBuild, defaultMainBuilding} from "@/hot-keys/RaceSpecificHotkeys/hotkeyData/commonHotkeys";
+import {
+    buildingWithCancel,
+    cancelBuild,
+    defaultMainBuilding,
+} from "@/hot-keys/RaceSpecificHotkeys/hotkeyData/commonHotkeys";
 
 const buildings =
     [
         new Building('Townhall', 'btntownhall', [
             new Ability('Train Peasant', 'btnpeasant', 'hpea', 'P', []),
             ...defaultMainBuilding,
-            new Ability('Upgrade to Keep', 'btnkeep', 'hkee', 'U', []),
+            //new Ability.DoubleHotkey('Upgrade to Keep', 'btnkeep', 'hkee', 'U', [], 'hcas'),
             new Ability('Call to Arms', 'btncalltoarms', 'amil', 'C', []),
-            Ability.Default(),
+            new Ability('Back to Work', 'btnbacktowork', 'amil', 'W', []),
+            cancelBuild
+        ]),
+        new Building('Barracks', 'btnhumanbarracks', [
+            new Ability('Train Footman', 'btnfootman', 'hfoo', 'F', []),
+            new Ability('Train Rifleman', 'btnrifleman', 'hrif', 'R', []),
+            new Ability('Train Knight', 'btnknight', 'hkni', 'K', []),
+            ...Ability.Defaults(3),
+            new Ability('Research Sundering Blades', 'btnsunderingblades', 'rhsb', 'B', []),
+            new Ability('Set Rally Point', 'btnrallypoint', 'cmdrally', 'Y', []),
+            new Ability('Research Defend', 'btndefend', 'rhde', 'D', []),
+            new Ability('Research Long Rifles', 'btndwarvenlongrifle', 'rhri', 'L', []),
+            new Ability('Research Animal War Training', 'btnanimalwartraining', 'rhan', 'A', []),
             cancelBuild
         ])
     ] as Unit[]
--- src/hot-keys/RaceSpecificHotkeys/hotkeyData/human/units.ts
+++ src/hot-keys/RaceSpecificHotkeys/hotkeyData/human/units.ts
@@ -5,28 +5,81 @@ import {
 } from "@/hot-keys/RaceSpecificHotkeys/raceSpecificHotkeyTypes";
 import {
     cancel,
-    defaultFighterAbilities,
+    defaultFighterAbilities, defaultSiegeAbilities,
     defaultUnitAbilities
 } from "@/hot-keys/RaceSpecificHotkeys/hotkeyData/commonHotkeys";
-
+//Ability.Default(),
 const units =
     [
         new Unit('Peasant', 'btnpeasant', [
             ...defaultUnitAbilities,
             new Ability('Repair', 'btnrepair', 'ahrp', 'F', []),
-            Ability.Default(),
+            new Ability('Lumber Harvest', 'pasbtnhumanlumberupgrade1', 'ahlh', '', []),
             new Ability('Gather', 'btngathergold', 'ahar', 'G', []),
             new Ability('Build', 'btnhumanbuild', 'cmdbuildhuman', 'B', [
                 new Ability('Build Townhall', 'btntownhall', 'htow', 'H', []),
+                new Ability('Build Barracks', 'btnhumanbarracks', 'hbar', 'B', []),
+                new Ability('Build Lumber Mill', 'btnhumanlumbermill', 'hlum', 'L', []),
+                new Ability('Build Blacksmith', 'btnblacksmith', 'hbla', 'S', []),
+                new Ability('Build Farm', 'btnfarm', 'hhou', 'F', []),
+                new Ability('Build Altar of Kings', 'btnaltarofkings', 'halt', 'A', []),
+                new Ability('Build Arcane Sanctum', 'btnarcanesanctum', 'hars', 'R', []),
+                new Ability('Build Workshop', 'btnworkshop', 'harm', 'W', []),
+                new Ability('Build Scout Tower', 'btnhumanwatchtower', 'hwtw', 'T', []),
+                new Ability('Build Gryphon Aviary', 'btngryphonaviary', 'hgra', 'V', []),
                 new Ability('Build Arcane Vault', 'btnarcanevault', 'hvlt', 'V', []),
                 cancel
             ]),
-            new Ability('Call to Arms', 'btncalltoarms', 'amil', 'C', [])
+            new Ability('Call to Arms', 'btncalltoarms', 'amil', 'C', []),
+            new Ability('Back to Work', 'btnbacktowork', 'amil', 'W', []),
+            cancel
         ]),
         new Unit('Footman', 'btnfootman', [
             ...defaultFighterAbilities,
             new Ability('Defend', 'btndefend', 'adef', 'D', []),
-        ])
+        ]),
+        new Unit('Rifleman', 'btnrifleman', [
+            ...defaultFighterAbilities,
+        ]),
+        new Unit('Knight', 'btnknight', [
+            ...defaultFighterAbilities,
+        ]),
+        new Unit('Priest', 'btnpriest', [
+            ...defaultFighterAbilities,
+            new Ability('Heal', 'btnhealoff', 'ahea', 'E', []),
+            new Ability('Dispel Magic', 'btndispelmagic', 'adis', 'D', []),
+            new Ability('Inner Fire', 'btninnerfireoff', 'ainf', 'F', []),
+        ]),
+        new Unit('Sorceress', 'btnsorceress', [
+            ...defaultFighterAbilities,
+            new Ability('Slow', 'btnslowoff', 'aslo', 'W', []),
+            new Ability('Invisibility', 'btninvisibility', 'aivs', 'I', []),
+            new Ability('Polymorph', 'btnpolymorph', 'aply', 'O', []),
+        ]),
+        new Unit('Spell Breaker', 'btnspellbreaker', [
+            ...defaultFighterAbilities,
+            new Ability('Spell Steal', 'btnspellstealoff', 'asps', 'T', []),
+            new Ability('Control Magic', 'btncontrolmagic', 'acmg', 'C', []),
+        ]),
+        new Unit('Flying Machine', 'btnflyingmachine', [
+            ...defaultFighterAbilities,
+        ]),
+        new Unit('Mortar Team', 'btnmortarteam', [
+            ...defaultSiegeAbilities,
+            new Ability('Flare', 'btnflare', 'afla', 'F', []),
+        ]),
+        new Unit('Siege Engine', 'btnseigeengine', [
+            ...defaultFighterAbilities,
+        ]),
+        new Unit('Gryphon Rider', 'btngryphonrider', [
+            ...defaultFighterAbilities,
+        ]),
+        new Unit('Dragonhawk Rider', 'btndragonhawkrider', [
+            ...defaultFighterAbilities,
+            new Ability('Aerial Shackles', 'btnmagiclariet', 'AMLS', 'E', []),
+            new Ability('Cloud', 'btncloudoffog', 'aclf', 'C', []),
+        ]),
+
     ] as Unit[]
 
 export default units;
