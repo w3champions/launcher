f4902c7c78ff62ce994589afedd9f201d04b7b91
Simon Heiss <simonheiss87@gmail.com>
2020-11-04 22:17:22 +0100

    neutral units and heros done

--- src/hot-keys/RaceSpecificHotkeys/hotkeyData/neutral/heroes.ts
+++ src/hot-keys/RaceSpecificHotkeys/hotkeyData/neutral/heroes.ts
@@ -3,18 +3,81 @@ import {
     Hero,
     Unit,
 } from "@/hot-keys/RaceSpecificHotkeys/raceSpecificHotkeyTypes";
-import {defaultHeroAbilities} from "@/hot-keys/RaceSpecificHotkeys/hotkeyData/commonHotkeys";
+import {defaultFighterAbilities, defaultHeroAbilities} from "@/hot-keys/RaceSpecificHotkeys/hotkeyData/commonHotkeys";
 
 const heroes =
     [
-        new Hero('Death Knight', 'btnherodeathknight', [
+        new Hero('Naga Sea Witch', 'btnnagaseawitch', [
             ...defaultHeroAbilities([
-                Ability.HeroAbility('Death Coil', 'btndeathcoil', 'audc', 'C', []),
-                Ability.HeroAbility('Death Pact', 'btndeathpact', 'audp', 'E', []),
-                Ability.Aura('Unholy Aura', 'pasbtnunholyaura', 'auau', 'U', []),
-                Ability.HeroAbility('Animate Dead', 'btnanimatedead', 'auan', 'D', []),
+                Ability.HeroAbility('Forked Lightning', 'btnmonsoon', 'anfl', 'F', []),
+                Ability.HeroAbility('Frost Arrows', 'btncoldarrowsoff', 'anfa', 'R', []),
+                Ability.HeroAbility('Mana Shield', 'btnneutralmanashield', 'anms', 'N', []),
+                Ability.HeroAbility('Tornado ', 'btntornado', 'anto', 'T', []),
             ])
         ]),
+
+        new Hero('Dark Ranger', 'btnbansheeranger', [
+            ...defaultHeroAbilities([
+                Ability.HeroAbility('Silence', 'btnsilence', 'ansi', 'E', []),
+                Ability.HeroAbility('Black Arrow', 'btntheblackarrowoff', 'anba', 'F', []),
+                Ability.HeroAbility('Life Drain', 'btnlifedrain', 'andr', 'D', []),
+                Ability.HeroAbility('Charm', 'btncharm', 'anch', 'C', []),
+            ])
+        ]),
+
+        new Hero('Pandaren Brewmaster', 'btnpandarenbrewmaster', [
+            ...defaultHeroAbilities([
+                Ability.HeroAbility('Breath of Fire', 'btnbreathoffire', 'anbf', 'F', []),
+                Ability.HeroAbility('Drunken Haze', 'btnstrongdrink', 'andh', 'R', []),
+                Ability.Aura('Drunken Brawler', 'pasbtndrunkendodge', 'andb', 'N', []),
+                Ability.HeroAbility('Storm, Earth, and Fire', 'btnstormearthfire', 'anef', 'T', []),
+            ])
+        ]),
+
+        new Hero('Beastmaster', 'btnbeastmaster', [
+            ...defaultHeroAbilities([
+                Ability.HeroAbility('Summon Bear', 'btngrizzlybear', 'ansg', 'B', []),
+                Ability.HeroAbility('Summon Quilbeast', 'btnquillbeast', 'ansq', 'Q', []),
+                Ability.HeroAbility('Summon Hawk', 'btnwareagle', 'answ', 'W', []),
+                Ability.HeroAbility('Stampede', 'btnstampede', 'anst', 'T', []),
+            ])
+        ]),
+
+        new Hero('Pit Lord', 'btnpitlord', [
+            ...defaultHeroAbilities([
+                Ability.HeroAbility('Rain of Fire', 'btnfire', 'anrf', 'F', []),
+                Ability.HeroAbility('Howl of Terror', 'btnhowlofterror', 'anht', 'T', []),
+                Ability.Aura('Cleaving Attack', 'pasbtncleavingattack', 'anca', 'C', []),
+                Ability.HeroAbility('Doom', 'btndoom', 'ando', 'D', []),
+            ])
+        ]),
+
+        new Hero('Goblin Tinker', 'btnherotinker', [
+            ...defaultHeroAbilities([
+                Ability.HeroAbility('Pocket Factory', 'btnpocketfactory', 'ansy', 'F', []),
+                Ability.HeroAbility('Cluster Rockets', 'btnclusterrockets', 'ancs', 'R', []),
+                Ability.Aura('Engineering Upgrade', 'pasbtnengineeringupgrade', 'aneg', 'E', []),
+                Ability.HeroAbility('Robo-Goblin', 'btnrobogoblin', 'anrg', 'B', []),
+            ])
+        ]),
+
+        new Hero('Firelord', 'btnheroavatarofflame', [
+            ...defaultHeroAbilities([
+                Ability.HeroAbility('Soul Burn', 'btnsoulburn', 'anso', 'B', []),
+                Ability.HeroAbility('Summon Lava Spawn', 'btnlavaspawn', 'anlm', 'W', []),
+                Ability.HeroAbility('Incinerate', 'btnincinerateoff', 'ania', 'C', []),
+                Ability.HeroAbility('Volcano', 'btnvolcano', 'anvc', 'V', []),
+            ])
+        ]),
+
+        new Hero('Goblin Alchemist', 'btnheroalchemist', [
+            ...defaultHeroAbilities([
+                Ability.HeroAbility('Healing Spray', 'btnhealingspray', 'anhs', 'E', []),
+                Ability.HeroAbility('Chemical Rage', 'btnchemicalrage', 'ancr', 'R', []),
+                Ability.HeroAbility('Acid Bomb', 'btnacidbomb', 'anab', 'B', []),
+                Ability.HeroAbility('Transmute', 'btntransmute', 'antm', 'T', []),
+            ])
+        ])
 
     ] as Unit[]
 
--- src/hot-keys/RaceSpecificHotkeys/hotkeyData/neutral/units.ts
+++ src/hot-keys/RaceSpecificHotkeys/hotkeyData/neutral/units.ts
@@ -5,6 +5,59 @@ import {
 import {defaultFighterAbilities, defaultUnitAbilities} from "@/hot-keys/RaceSpecificHotkeys/hotkeyData/commonHotkeys";
 
 const units =
-    [] as Unit[]
+    [
+        new Unit('Forest Troll Shadow Priest', 'btnforesttrollshadowpriest', [
+            ...defaultFighterAbilities,
+            new Ability('Heal', 'btnhealoff', 'anh1', 'E', []),
+            new Ability('Abolish Magic', 'btndryaddispelmagicoff', 'acdm', 'B', []),
+        ]),
+        new Unit('Mud Golem', 'btnmudgolem', [
+            ...defaultFighterAbilities,
+            new Ability('Slow', 'btnslowoff', 'acsw', 'W', []),
+        ]),
+        new Unit('Goblin Shredder', 'btnjunkgolem', [
+            ...defaultUnitAbilities,
+            ...Ability.Defaults(2),
+            new Ability('Gather', 'btngathergold', 'ahr3', 'G', []),
+        ]),
+        new Unit('Goblin Sappers', 'btngoblinsapper', [
+            ...defaultFighterAbilities,
+            new Ability('Kaboom!', 'btnselfdestructoff', 'asds', 'B', []),
+        ]),
+        new Unit('Goblin Zeppelin', 'btngoblinzeppelin', [
+            ...defaultFighterAbilities,
+            new Ability('Load', 'btnload', 'aloa', 'L', []),
+            new Ability('Unload All', 'btnunload', 'adro', 'U', []),
+        ]),
+        new Unit('Storm (Pandaren Ultimate)', 'btnstormearthfire', [
+            ...defaultFighterAbilities,
+            new Ability('Wind Walk', 'btnwindwalkon', 'anwk', 'W', []),
+            new Ability('Cyclone', 'btncyclone', 'accy', 'C', []),
+            new Ability('Dispel Magic', 'btndispelmagic', 'adsm', 'D', []),
+        ]),
+        new Unit('Earth (Pandaren Ultimate)', 'btnstormearthfire', [
+            ...defaultFighterAbilities,
+            new Ability('TaunT', 'btnpandataunt', 'anta', 'T', []),
+        ]),
+        new Unit('Spirit Bear (Level 3)', 'btnspiritbear', [
+            ...defaultFighterAbilities,
+            new Ability('Blink', 'btnbearblink', 'anbl', 'B', []),
+        ]),
+        new Unit('Quilbeast (Level 2/3)', 'btnquillbeastlv2', [
+            ...defaultFighterAbilities,
+            new Ability('Frenzy', 'btnbloodlustoff', 'afzy', 'F', []),
+        ]),
+        new Unit('Doom Guard', 'btndoomguard', [
+            ...defaultFighterAbilities,
+            new Ability('Dispel Magic', 'btndispelmagic', 'adsm', 'D', []),
+            new Ability('War Stomp', 'btnwarstomp', 'awrs', 'T', []),
+            new Ability('Cripple', 'btncripple', 'accr', 'C', []),
+            new Ability('Rain of Fire', 'btnfire', 'acrf', 'F', []),
+        ]),
+        new Unit('Cloak of Shadows', 'btncloak', [
+            ...Ability.Defaults(6),
+            new Ability('Hide', 'btncloak', 'aihm', 'I', []),
+        ])
+    ] as Unit[]
 
 export default units;
