8ea6f44f33db22a633f3f17dca85c404368358a2
Simon Heiss <simonheiss87@gmail.com>
2020-12-12 15:20:49 +0100

    fixed bug where fab was created twice

--- src/background.ts
+++ src/background.ts
@@ -149,18 +149,13 @@ if (!gotTheLock) {
     }
     createWindow();
     createTray();
-    await createFab();
   });
 
-  app.on('activate', async () => {
+  app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
       createWindow()
-    }
-
-    if (fab === null) {
-      await createFab()
     }
   })
 }
