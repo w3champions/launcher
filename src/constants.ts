9fe14d3562ceb82c0f743502365dd11d1169c6ac
Simon Heiss <simonheiss87@gmail.com>
2021-01-10 10:24:36 +0100

    new base urls for launcher

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const UPDATE_URL_PROD = "https://update-service.prod.test.w3champions.com/"
+export const UPDATE_URL_PROD = "https://update-service.w3champions.com/"
 export const UPDATE_URL_TEST = "https://update-service.test.w3champions.com/"
 
 export const NEWS_URL_PROD = "https://statistic-service.w3champions.com/"
-export const NEWS_URL_TEST = "https://statistic-service-test.w3champions.com/"
+export const NEWS_URL_TEST = "https://statistic-service.test.w3champions.com/"
 
-export const IDENTIFICATION_URL_PROD = "https://identification-service.prod.test.w3champions.com/"
+export const IDENTIFICATION_URL_PROD = "https://identification-service.w3champions.com/"
 export const IDENTIFICATION_URL_TEST = "https://identification-service.test.w3champions.com/"
