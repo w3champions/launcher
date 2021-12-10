51a650c04e47e9c11b2baba792bd4a32879b8ec2
SC <pleinair00@gmail.com>
2021-12-09 22:10:26 -0500

    changing (most) css sizes to percentages

--- src/home/HeadLine.vue
+++ src/home/HeadLine.vue
@@ -25,24 +25,24 @@ export default class HeadLine extends Vue {
 <style scoped type="text/css">
 .headline-container {
   position: absolute;
-  top: 60px;
-  left: 300px;
+  top: 5%;
+  left: 18%;
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
   align-items: center;
-
-  width: 671px;
-  height: 71px;
+  padding:1%;
+  width: 66%;
+  height: 7%;
 
   background: url("~@/assets/images/home/Header_Buttons_Frame.png") center no-repeat;
   background-size: cover;
 }
 
 .headline-wrapper {
-  position: absolute;
+  position: inline;
   width: 100%;
-  height: 150px;
+  height: 15%;
   -webkit-user-select: none;
   -webkit-app-region: drag;
 }
