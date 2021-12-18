22ed6f03e4348795cb9ab8b30a5bb9ced0da7a75
Flux Xu <fluxxu@gmail.com>
2021-12-17 19:21:55 -0500

    refactor: override webui zip name

--- src/background-thread/endpoint/endpoint.service.ts
+++ src/background-thread/endpoint/endpoint.service.ts
@@ -8,7 +8,7 @@ export interface IEndpoint {
   identificationUrl: string;
   floControllerHost: string;
   overrideUpdateFileDownloadUrl?: string;
-  overrideIngameJsUrl?: string,
+  overrideRemoteWebUIFileName?: string,
 }
 
 const ENDPOINTS_PROD: IEndpoint[] = [
@@ -26,7 +26,7 @@ const ENDPOINTS_PROD: IEndpoint[] = [
     identificationUrl: "http://123.57.23.5:25051/",
     floControllerHost: "ea2d46.pathx.ucloudgda.com",
     overrideUpdateFileDownloadUrl: "https://w3champions.oss-cn-shanghai.aliyuncs.com/update-service-content/",
-    overrideIngameJsUrl: "https://w3champions.oss-cn-shanghai.aliyuncs.com/ingame/w3champions.js"
+    overrideRemoteWebUIFileName: "china-webui.zip"
   },
 ];
 
@@ -44,7 +44,7 @@ export class EndpointService {
   private window: BrowserWindow | null = null
 
   constructor() {
-    ipcMain.handle('w3c-select-endpoint', async (evt: unknown, isTest: boolean) => {
+    ipcMain.handle('w3c-select-endpoint', async (_evt: unknown, isTest: boolean) => {
       try {
         const endpoint = await this.selectFastestEndpoint(isTest);
         this.window?.webContents.send('w3c-endpoint-selected', endpoint);
@@ -53,14 +53,6 @@ export class EndpointService {
         console.log(e);
       }
     });
-
-    ipcMain.handle('w3c-write-webui', (evt: unknown, path, url) => {
-      try {
-        fs.writeFileSync(path, this.getWebUISrc(url))
-      } catch(e) {
-        console.log(e);
-      }
-    })
   }
 
   public setWindow(window: BrowserWindow) {
@@ -74,44 +66,11 @@ export class EndpointService {
     const endpoints = isTest ? TEST_ENDPOINTS : ENDPOINTS_PROD;
     selected = await Promise.any(
       endpoints.map(async (i) => {
-        // await fetch(`${i.updateUrl}api/client-version`);
+        await fetch(`${i.updateUrl}api/client-version`);
         return i;
       })
     );
     return selected;
-  }
-
-  private getWebUISrc(url: string) {
-    return `<!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf-8" />
-            <title>Warcraft 3 UI</title>
-            <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, minimal-ui" />
-            <script>
-                window.__DEBUG = new Boolean('').valueOf();
-            </script>
-        </head>
-        <body>
-            <div id="root"></div>
-            <div id="portal"></div>
-        
-        <script>
-          var logCalls = [];
-          var w3cClientVersion = 1;
-          console.origLog = console.log;
-          console.log = (...args) => {
-            logCalls.push(...args);
-            console.origLog("log");
-            console.origLog(...args);
-          }
-    
-        </script>
-            <script src="GlueManager.js"></script>
-        <script src="${url}"></script>
-        </body>
-    </html>
-    `
   }
 }
 
