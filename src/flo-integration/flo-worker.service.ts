df38563dec328cfbf10028bb57acabd9afe3d605
Flux Xu <fluxxu@gmail.com>
2021-12-18 20:47:34 -0500

    final touch up

--- src/flo-integration/flo-worker.service.ts
+++ src/flo-integration/flo-worker.service.ts
@@ -15,7 +15,6 @@ const path = require('path');
 const fs = window.require("fs");
 const { exec } = window.require("child_process");
 const { ipcRenderer } = window.require('electron')
-const sudo = window.require("sudo-prompt");
 
 export class FloWorkerService {
     private store = store;
@@ -116,18 +115,6 @@ export class FloWorkerService {
         for (const worker of this.workers) {
             worker.gameExited();
         }
-    }
-
-    public addWindowsFirewallRule() {
-        const settings = this.createWorkerSettings();
-        logger.info("flo worker: " + settings.floWorkerExePath);
-        sudo.exec(`netsh firewall add allowedprogram "${settings.floWorkerExePath}" "FLO" ENABLE`, {
-            name: 'Warcraft 3 Champions',
-        }, (err: Error) => {
-            if (err) {
-                logger.error(err)
-            }
-        });
     }
 
     private reloadWorkers(isTest: boolean) {
