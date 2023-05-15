66226a55003c7475f19b6773ed1682af3bb8bfc5
Gustav Andersson <gustav87and@gmail.com>
2023-05-15 13:54:16 +0200

    Make scroll bar visible in news (#148)
    * Make scroll bar visible in news
    
    * Remove old class

--- src/home/NewsBanner.vue
+++ src/home/NewsBanner.vue
@@ -8,7 +8,7 @@
       <div class="w3font news-header">{{ selectedNewsDate }}</div>
       <div class="news-banner">
           <div class="news-message-box">
-            <div class="news-content panel-scroll" v-if="isNewsHtml" v-html="selectedNewsMessage"></div>
+            <div class="news-content" v-if="isNewsHtml" v-html="selectedNewsMessage"></div>
             <vue-markdown v-else :source="selectedNewsMessage" />
           </div>
       </div>
@@ -135,6 +135,15 @@ export default class NewsBanner extends Vue {
 }
 
 .news-content {
-  height: 98%;
+  height: 99%;
+  overflow-y: scroll;
+  scroll-behavior: smooth;
+  overflow-x: hidden;
+}
+
+.news-content::-webkit-scrollbar-thumb {
+    background-color: #ccc;
+    border: 4px solid transparent;
+    background-clip: content-box;
 }
 </style>
