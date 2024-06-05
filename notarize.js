90040b8aebf00beb2e7ab2c7f15d7fc59efe8321
Bogdan Gligorijević <bogdangligorijevic3@gmail.com>
2024-06-05 22:49:06 +0200

    Try updating to new notarize

--- notarize.js
+++ notarize.js
@@ -1,4 +1,4 @@
-const { notarize } = require('electron-notarize');
+const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
     const { electronPlatformName, appOutDir } = context;
@@ -9,9 +9,9 @@ exports.default = async function notarizing(context) {
     const appName = context.packager.appInfo.productFilename;
 
     return await notarize({
-        appBundleId: 'W3Champions',
         appPath: `${appOutDir}/${appName}.app`,
         appleId: "xuke.ca@hotmail.com",
         appleIdPassword: process.env.APP_PW,
+        teamId: process.env.APPLE_TEAM_ID,
     });
 };
